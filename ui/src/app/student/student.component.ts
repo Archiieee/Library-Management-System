3c67104487c8933ef81f63ca960349a641f3c726
Archiieee <archishman02.c@gmail.com>
2023-03-12 23:55:40 +0530

    component updates

--- ui/src/app/student/student.component.ts
+++ ui/src/app/student/student.component.ts
@@ -6,7 +6,24 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./student.component.css']
 })
 export class StudentComponent implements OnInit {
-
+  students = [
+    {
+      "id": 33,
+      "name": "Student1",
+      "department": "Dep1",
+      "rollNumber": "11",
+      "birthDate": "03/02/2004",
+      "mobileNumber": "123456"
+    },
+    {
+      "id": 34,
+      "name": "Student2",
+      "department": "Dep2",
+      "rollNumber": "18",
+      "birthDate": "03/12/2000",
+      "mobileNumber": "12346"
+    }
+  ];
   constructor() { }
 
   ngOnInit(): void {
