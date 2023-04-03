8b6021c72ea42ed495d63a3627a850885027744c
Archiieee <archishman02.c@gmail.com>
2023-04-04 00:46:55 +0530

    components added

--- ui/src/app/add-books/add-books.component.ts
+++ ui/src/app/add-books/add-books.component.ts
@@ -1,7 +1,7 @@
 import { Component, OnInit } from '@angular/core';
 import { FormBuilder , FormGroup, Validators} from '@angular/forms';
 import { Router } from '@angular/router'; //
-import { HttpClient } from '@angular/common/http'; //
+import { HttpClient } from '@angular/common/http'; ////
 @Component({
   selector: 'app-add-books',
   templateUrl: './add-books.component.html',
--- ui/src/app/student/student.component.ts
+++ ui/src/app/student/student.component.ts
@@ -1,32 +1,44 @@
 import { Component, OnInit } from '@angular/core';
-
+import { Router } from '@angular/router';
+import { HttpClient } from '@angular/common/http';
 @Component({
   selector: 'app-student',
   templateUrl: './student.component.html',
   styleUrls: ['./student.component.css']
 })
 export class StudentComponent implements OnInit {
-  students = [
-    {
-      "id": 33,
-      "name": "Student1",
-      "department": "Dep1",
-      "rollNumber": "11",
-      "birthDate": "03/02/2004",
-      "mobileNumber": "127456"
-    },
-    {
-      "id": 34,
-      "name": "Student2",
-      "department": "Dep2",
-      "rollNumber": "18",
-      "birthDate": "03/12/2000",
-      "mobileNumber": "123746"
-    }
-  ];
-  constructor() { }
+  title = 'Students Page'
+  students: any = [];
+  //constructor() { }
+  constructor(private router: Router,  private http: HttpClient) { }
 
   ngOnInit(): void {
+    this.fetchAllStudents()
+  }
+  addStudents(){
+    console.log("addStudentsbutton clicked")
+    //take user to add-books url
+    this.router.navigateByUrl('/add-students')
+  }
+  fetchAllStudents(){
+    this.http.get("http://localhost:8080/student/getAllStudents")
+    .subscribe(resp =>{
+      this.students = resp;
+      console.log('Students retrieved successfully:', this.students)
+    }, error => {
+      console.error('Error retrieving students:', error);
+    });
+  }
+    deleteStudent(Id:Number){
+      const url = 'http://localhost:8080/loan/delete' + Id
+      console.log(url)
+      this.http.delete(url)
+      .subscribe(resp => {
+        console.log('Student deleted successfully');
+        this.fetchAllStudents()
+      }, error => {
+        console.error('Error deleting student:', error);
+      });
   }
 
 }
